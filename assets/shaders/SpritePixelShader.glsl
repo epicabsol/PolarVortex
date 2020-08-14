76cb80060c69045639390ef2a6f40cf2bde4ada0
Ben Garcia <pokeboygarcia@gmail.com>
2020-08-14 03:03:16 -0700

    Fixes cutout transparency.

--- assets/shaders/SpritePixelShader.glsl
+++ assets/shaders/SpritePixelShader.glsl
@@ -10,5 +10,9 @@ void main()
 {
     //FragColor = vec4(0.9f, 0.5f, 0.2f, 1.0f);
     //FragColor = vec4(PixelUV.x, PixelUV.y, 0.0f, 1.0f);
-    FragColor = texture(Texture, PixelUV);
+    vec4 textureColor = texture(Texture, PixelUV);
+    if (textureColor.a < 0.01f) {
+        discard;
+    }
+    FragColor = textureColor;
 }
